f6a0107ddbdefcdabc935128f24b6ee36f278c0e
Liviana <liviana.deisvaldi@gmail.com>
2025-06-02 16:50:15 +0200

    reference in HumanA ex03

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -1,12 +1,11 @@
 #include "HumanA.hpp"
 
-HumanA::HumanA(const std::string name, Weapon &weapon)
+HumanA::HumanA(const std::string name, Weapon &weapon) : _weapon(weapon)
 {
-	this->_weapon = &weapon;
 	this->_name = name;
 }
 
 void	HumanA::attack(void)
 {
-	std::cout << this->_name << " attacks with their " << _weapon->getType() << "\n";
+	std::cout << this->_name << " attacks with their " << _weapon.getType() << "\n";
 }

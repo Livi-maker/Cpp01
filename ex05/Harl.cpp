1d42563e81ef86b009ae3f828e745bcb9dfdcc3e
Liviana Dei Svaldi <ldei-sva@e3r3p8.42roma.it>
2025-06-04 20:04:30 +0200

    ex05

---
+++ ex05/Harl.cpp
@@ -0,0 +1,51 @@
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	levels[0] = "debug";
+	levels[1] = "info";
+	levels[2] = "warning";
+	levels[3] = "error";
+	functions[0] = &Harl::debug;
+	functions[1] = &Harl::info;
+	functions[2] = &Harl::warning;
+	functions[3] = &Harl::error;
+}
+
+Harl::~Harl(void)
+{}
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month.\n";
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void	Harl::complain(std::string level)
+{
+	int i;
+
+	for (i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+		{
+			(this->*functions[i])();
+			return ;
+		}
+	}
+	std::cout << "level not found\n";
+}
